9f41797724ab2234d2cd4ae9efe69625b0ca0559
Jeevan Basnet <jeevan.basnet@selu.edu>
2020-04-17 21:16:40 -0500

    removing ribbonconf

--- Limits-Currency-Calculation-Service/src/main/java/org/currency/currencyconversionservice/proxy/CurrencyExchangeServiceFeignClient.java
+++ Limits-Currency-Calculation-Service/src/main/java/org/currency/currencyconversionservice/proxy/CurrencyExchangeServiceFeignClient.java
@@ -1,36 +1,36 @@
-	//package org.currency.currencyconversionservice.proxy;
+//	package org.currency.currencyconversionservice.proxy;
+//	
+//	import org.currency.currencyconversionservice.model.CurrencyConversionBean;
+//	import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//	import org.springframework.cloud.openfeign.FeignClient;
+//	import org.springframework.web.bind.annotation.PathVariable;
+//	import org.springframework.web.bind.annotation.RequestHeader;
+//	import org.springframework.web.bind.annotation.RequestMapping;
+//	import org.springframework.web.bind.annotation.RequestMethod;
+//	
+//	//@FeignClient(name = "currency-exchange-service")
+//	//@FeignClient(name="netflix-zuul-api-gateway")
+//	@FeignClient(name="zuulserver")
+//	//@RibbonClient(name = "currency-exchange-service")
+//	@RibbonClient(name="exchangeservice") //, configuration=RibbonConfiguration.class)
+//	public interface CurrencyExchangeServiceFeignClient {
+//	
+//		@RequestMapping(method = RequestMethod.GET, value = "/currency-exchange/currency-exchange/from/{from}/to/{to}")
+//		public CurrencyConversionBean retrieveExchangeValue(
+//				@RequestHeader(value = "Authorization") String authorizationHeader,
+//				@PathVariable("from") String from,
+//				@PathVariable("to") String to);
+//	}
+//	
+	
 	//
-	//import org.currency.currencyconversionservice.model.CurrencyConversionBean;
-	//import org.springframework.cloud.netflix.ribbon.RibbonClient;
-	//import org.springframework.cloud.openfeign.FeignClient;
-	//import org.springframework.web.bind.annotation.PathVariable;
-	//import org.springframework.web.bind.annotation.RequestHeader;
-	//import org.springframework.web.bind.annotation.RequestMapping;
-	//import org.springframework.web.bind.annotation.RequestMethod;
+	//class HystrixClientFallback implements CurrencyExchangeServiceFeignClient {
 	//
-	////@FeignClient(name = "currency-exchange-service")
-	////@FeignClient(name="netflix-zuul-api-gateway")
-	//@FeignClient(name="zuulserver")
-	////@RibbonClient(name = "currency-exchange-service")
-	//@RibbonClient(name="exchangeservice") //, configuration=RibbonConfiguration.class)
-	//public interface CurrencyExchangeServiceFeignClient {
 	//
-	//	@RequestMapping(method = RequestMethod.GET, value = "/currency-exchange/currency-exchange/from/{from}/to/{to}")
-	//	public CurrencyConversionBean retrieveExchangeValue(
-	//			@RequestHeader(value = "Authorization") String authorizationHeader,
-	//			@PathVariable("from") String from,
-	//			@PathVariable("to") String to);
+	//	@Override
+	//	public CurrencyConversionBean retrieveExchangeValue(String authorizationHeader, String from, String to) {
+	//		// TODO Auto-generated method stub
+	//		return ConsumeRestOrFallBack.getDefaultStaticConversionResponse(from, to, BigDecimal.ONE);
+	//	}
+	//
 	//}
-	//
-	//
-	////
-	////class HystrixClientFallback implements CurrencyExchangeServiceFeignClient {
-	////
-	////
-	////	@Override
-	////	public CurrencyConversionBean retrieveExchangeValue(String authorizationHeader, String from, String to) {
-	////		// TODO Auto-generated method stub
-	////		return ConsumeRestOrFallBack.getDefaultStaticConversionResponse(from, to, BigDecimal.ONE);
-	////	}
-	////
-	////}
